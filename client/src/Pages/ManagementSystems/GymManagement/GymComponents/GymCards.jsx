6cd815b9a51f6fbea2fd57c18af5b0765dedd6f7
Muhammad Taha <tahafaisal9292@gmail.com>
2024-10-15 16:34:28 +0500

    GymCards was turn into resueable component

--- client/src/Pages/ManagementSystems/GymManagement/GymComponents/GymCards.jsx
+++ client/src/Pages/ManagementSystems/GymManagement/GymComponents/GymCards.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const GymCards = () => {
+const GymCards = ({heading,number}) => {
   return (
     <div className='flex flex-col mx-10 p-3 px-8 border-[1px] bg-blue-200 border-gray-400 rounded-xl hover:bg-blue-300'>
 
         <div className="">
-          <h1 className='font-semibold'>Total Students</h1>
-          <h3>20</h3>
+          <h1 className='font-semibold'>{heading}</h1>
+          <h3>{number}</h3>
         </div>
 
     </div>
