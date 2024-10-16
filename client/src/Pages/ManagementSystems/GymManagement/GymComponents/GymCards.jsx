893f12e18e5abadec07028ae33bf41c7a74d9c6f
Muhammad Taha <tahafaisal9292@gmail.com>
2024-10-17 00:20:08 +0500

    Some minor changes in GymCards

--- client/src/Pages/ManagementSystems/GymManagement/GymComponents/GymCards.jsx
+++ client/src/Pages/ManagementSystems/GymManagement/GymComponents/GymCards.jsx
@@ -5,7 +5,7 @@ const GymCards = ({heading,number,title}) => {
     <div className='flex flex-col p-3 px-8 border-[1px] bg-blue-200 border-gray-400 rounded-xl hover:bg-blue-300'>
 
         <div className="" title={title}>
-          <h1 className='font-semibold'>{heading}</h1>
+          <h1 className='font-semibold whitespace-nowrap'>{heading}</h1>
           <h3>{number}</h3>
         </div>
 
